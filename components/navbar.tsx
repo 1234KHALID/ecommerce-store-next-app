521bdbd72f126a3022544bc6d594b76f64839439
Mohd Khalid <mohammad@prodios.com>
2024-03-14 15:23:03 +0530

    create navbar actions component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Container from "./ui/container";
 import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "@/components/navbar-actions";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -14,6 +15,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions/>
         </div>
       </Container>
     </div>
