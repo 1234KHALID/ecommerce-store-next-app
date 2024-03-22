2971056b585f7b3509bfe7f96b4b981f29edad6a
Mohd Khalid <mohammad@prodios.com>
2024-03-22 22:31:01 +0530

    add product list component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,21 @@
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 import Billboards from "@/components/ui/billboard";
 
 import Container from "@/components/ui/container";
 
 const HomePage=async()=> {
   const products=await getProducts({isFeatured:true})
-  const billboad= await getBillboard("65f179e1320677057f8af129")
+  const billboad= await getBillboard("65f94107f12a5288c4164296")
+  
   return (
  <Container>
-  <div>
+  <div className="space-y-10 pb-10">
     <Billboards data={billboad}/>
+  </div>
+  <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+    <ProductList title="Featured Products" items={products}/>
   </div>
  </Container>
   )
