0e14f0883eb18755b518331fefa7f679db389882
Mohd Khalid <mohammad@prodios.com>
2024-03-26 09:32:22 +0530

    Add preview modal  provider & toast provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,32 @@
-import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 import Navbar from "@/components/navbar";
-import { Inter } from "next/font/google";
+import Footer from "@/components/footer";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Store",
-  description: "Store",
+  description: "Store - The place for all your purchases.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
